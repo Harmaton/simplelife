37af05403f6fbb981c0dc2d47f2c390467b9eba6
Harmaton <njagiiharmaton@gmail.com>
2024-09-10 22:41:11 +0300

    refactor: removed the script

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ const fontBody = Space_Mono({
   weight: '400'
 })
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -33,27 +34,18 @@ export default function RootLayout({
 }>) {
   return (
     <AuthProvider>
-      <html lang="en" className="scrollbar-thumb-sky-700 scrollbar-track-sky-200 scrollbar-thin overflow-y-scroll">
-        <body 
-          className={cn(
-            'antialiased',
-            fontHeading.variable,
-            fontBody.variable
-          )}
-        >
-          {children}
-          <Toaster />
-          <script type="text/javascript">
-            {`
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "o15wughjhe");
-            `}
-          </script>
-        </body>
-      </html>
+    <html lang="en" className="scrollbar-thumb-sky-700 scrollbar-track-sky-200 scrollbar-thin overflow-y-scroll">
+    <body 
+        className={cn(
+          'antialiased',
+          fontHeading.variable,
+          fontBody.variable
+        )}
+      >
+       {children}
+      <Toaster />
+      </body>
+    </html>
     </AuthProvider>
   );
 }
