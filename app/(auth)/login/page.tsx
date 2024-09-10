3711cbdbbd9ef16d8e0dc8560215e44df53e31a6
Harmaton <njagiiharmaton@gmail.com>
2024-09-10 14:27:09 +0300

    [AUTH-UPDATE]: added oAuth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,10 +3,11 @@
 
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Image from 'next/image'
 import Link from "next/link";
 import { auth } from '@/firebase'
+import { toast } from "sonner";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -40,18 +41,52 @@ export default function Login() {
     }
   };
 
+  const handleGoogleLogin = async () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+      
+      toast.success('Inicio de sesión con Google exitoso');
+      
+      // Redirect to the desired page after successful login
+      router.push("/dashboard"); // Change this to your desired route
+    } catch (error) {
+      if (error instanceof Error) {
+        // Handle Errors here.
+        const errorCode = (error as any).code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = (error as any).customData?.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error as any);
+        
+        console.error("Google login error", errorCode, errorMessage);
+        toast.error('Error en el inicio de sesión con Google');
+        setError(errorMessage);
+      } else {
+        setError("An unexpected error occurred during Google login");
+        toast.error('Error inesperado en el inicio de sesión con Google');
+      }
+    }
+  };
+
   return (
     <div className="bg-background min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <Link href={'/'} >
           <Image src={'/logo-1.png'} className="m-auto" width={100} height={100} alt={'logo'} />
         </Link>
-        <h2 className="text-2xl font-bold mb-6 text-center">Log In</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
         {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
         <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
+              Correo Electrónico
             </label>
             <input
               type="email"
@@ -64,7 +99,7 @@ export default function Login() {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+              Contraseña
             </label>
             <input
               type="password"
@@ -79,24 +114,42 @@ export default function Login() {
             type="submit"
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
           >
-            Log In
+            Iniciar Sesión
           </button>
         </form>
-        <div className="flex flex-row space-x-4">
-          <p className="text-sm text-center mt-4 " >
-            Don&apos;t have an account?{" "}
-            <a href="/sign-up" className="text-blue-600 hover:underline">
-              Sign Up
+        <div className="flex items-center my-4">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <span className="flex-shrink mx-4 text-gray-600">o</span>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
+        <button
+          onClick={handleGoogleLogin}
+          className="w-full bg-white text-gray-700 border border-gray-300 py-2 px-4 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500 flex items-center justify-center"
+        >
+          <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo" className="w-5 h-5 mr-2" />
+          Iniciar sesión con Google
+        </button>
+        <div className="flex flex-col items-center mt-4">
+          <p className="text-sm text-center">
+            ¿Ya tienes una cuenta?{" "}
+            <a href="/login" className="text-blue-600 hover:underline">
+              Iniciar Sesión
             </a>
           </p>
-          <p className="text-sm text-center mt-4">
-            Forgot Your Password?{" "}
+          <p className="text-sm text-center mt-2">
+            ¿No tienes una cuenta?{" "}
+            <a href="/sign-up" className="text-blue-600 hover:underline">
+              Registrarse
+            </a>
+          </p>
+          <p className="text-sm text-center mt-2">
+            ¿Olvidaste tu contraseña?{" "}
             <a href="/reset-password" className="text-blue-600 hover:underline">
-              Reset Password
+              Restablecer Contraseña
             </a>
           </p>
         </div>
       </div>
-    </div>
+      </div>
   );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,13 @@
 'use client'
+
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Image from 'next/image'
 import Link from "next/link";
+import { toast } from "sonner";
+import { db } from "@/lib/db";
+
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -16,7 +20,23 @@ export default function Signup() {
 
     const auth = getAuth();
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+     const userauth = await createUserWithEmailAndPassword(auth, email, password);
+
+     const user = userauth.user;
+     if(user && user.email){
+       await db.user.create({
+         data: {
+           clerkId: user.uid,
+           email: user.email,
+           isadmin: false,
+           isTeacher: false,
+           isStudent: true,
+           createdAt: new Date(),
+           updatedAt: new Date(),
+         },
+       });
+     }
+
       router.push("/login");
     } catch (err) {
       if (err instanceof Error) {
@@ -27,18 +47,64 @@ export default function Signup() {
     }
   };
 
+  const handleGoogleSignup = async () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+      if(user && user.email){
+        await db.user.create({
+          data: {
+            clerkId: user.uid,
+            email: user.email,
+            isadmin: false,
+            isTeacher: false,
+            isStudent: true,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        });
+      }
+      // Add user details to the database
+      
+      toast.success('Cuenta creada');
+      // Redirect to the desired page after successful sign-up
+      router.push("/dashboard"); // Change this to your desired route
+    } catch (error) {
+      if (error instanceof Error) {
+        // Handle Errors here.
+        const errorCode = (error as any).code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = (error as any).customData?.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error as any);
+        
+        console.error("Google sign-up error", errorCode, errorMessage);
+        toast.success('Error en el registro con Google')
+        setError(errorMessage);
+      } else {
+        setError("An unexpected error occurred during Google sign-up");
+      }
+    }
+  };
+
   return (
     <div className="bg-background min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <Link href={'/'} >
-      <Image src={'/logo-1.png'} className="m-auto" width={100} height={100} alt={'logo'} />
-      </Link>
-        <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+          <Image src={'/logo-1.png'} className="m-auto" width={100} height={100} alt={'logo'} />
+        </Link>
+        <h2 className="text-2xl font-bold mb-6 text-center">Registrarse</h2>
         {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
         <form onSubmit={handleSignup}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
+              Correo Electrónico
             </label>
             <input
               type="email"
@@ -51,7 +117,7 @@ export default function Signup() {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+              Contraseña
             </label>
             <input
               type="password"
@@ -66,13 +132,25 @@ export default function Signup() {
             type="submit"
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
           >
-            Sign Up
+            Registrarse
           </button>
         </form>
+        <div className="flex items-center my-4">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <span className="flex-shrink mx-4 text-gray-600">o</span>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
+        <button
+          onClick={handleGoogleSignup}
+          className="w-full bg-white text-gray-700 border border-gray-300 py-2 px-4 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-500 flex items-center justify-center"
+        >
+          <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo" className="w-5 h-5 mr-2" />
+          Registrarse con Google
+        </button>
         <p className="text-sm text-center mt-4">
-          Already have an account?{" "}
+          ¿Ya tienes una cuenta?{" "}
           <a href="/login" className="text-blue-600 hover:underline">
-            Log In
+            Iniciar Sesión
           </a>
         </p>
       </div>
