7ef90adc62611b56733879f218810065ea364da3
Harmaton <njagiiharmaton@gmail.com>
2024-09-17 19:10:27 +0300

    updated page data

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,35 +1,7 @@
-import { db } from "@/lib/db";
-import { redirect } from "next/navigation";
+import React from 'react'
 
-const CourseIdPage = async ({
-  params
-}: {
-  params: { courseId: string; }
-}) => {
-  const course = await db.course.findUnique({
-    where: {
-      id: params.courseId,
-      startDate: {
-        not: null
-      },
-      imageUrl: {
-        not: null
-      }
-    },
-    include: {
-      Chapter: {
-        orderBy: {
-          position: "asc"
-        }
-      }
-    }
-  });
-
-  if (!course || course.Chapter.length === 0) {
-    return redirect("/");
-  }
-  
-  return redirect(`/courses/${course.id}/chapters/${course.Chapter[0].id}`);
+export default function page() {
+  return (
+    <div>Course Page Coming</div>
+  )
 }
-
-export default CourseIdPage;
