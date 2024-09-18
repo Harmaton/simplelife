cd55d7b40e2b4ac00d9e3cdacbc4ac6b3740d01e
Harmaton <njagiiharmaton@gmail.com>
2024-09-18 15:44:38 +0300

    Certs new page

--- app/(admin)/admin/certifications/page.tsx
+++ app/(admin)/admin/certifications/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { GetCategoryNames } from "@/app/actions/categories";
+import React from "react";
+import { DataTable } from "../categories/_components/data-table";
+import { columns } from "../categories/_components/columns";
 
-export default function page() {
+export default async function Page() {
+  const categories = await GetCategoryNames();
   return (
-    <div>
-
-
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-center my-4 text-3xl font-bold">
+          Administrar Certificaciones
+        </h1>
+        <div className="p-4 flex flex-col md:flex-row space-y-4 md:space-y-2">
+          <DataTable columns={columns} data={categories} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -115,6 +115,13 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
       ),
     },
     {
+      label: "certificaciones",
+      href: "/admin/certifications",
+      icon: (
+        <LineChart className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
       label: "Administrar eventos",
       href: "/admin/inperson",
       icon: (
