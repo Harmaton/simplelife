e82659d24b7bcf90c23291cefac08e494e67b66a
Harmaton <njagiiharmaton@gmail.com>
2024-09-18 13:25:56 +0300

    Update course validation

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,51 +1,7 @@
-import { db } from "@/lib/db";
-import { redirect } from "next/navigation";
+import React from 'react'
 
-const CourseIdPage = async ({
-  params
-}: {
-  params: { courseId: string; }
-}) => {
-  // First, find the course by ID
-  const course = await db.course.findUnique({
-    where: {
-      id: params.courseId
-    }
-  });
-
-  // If the course doesn't exist, redirect
-  if (!course) {
-    return redirect("/");
-  }
-
-  // Now, use findFirst with additional conditions
-  const validCourse = await db.course.findFirst({
-    where: {
-      id: params.courseId,
-      startDate: {
-        not: null
-      },
-      imageUrl: {
-        not: null
-      }
-    },
-    include: {
-      Chapter: {
-        where: {
-          isPublished: true
-        },
-        orderBy: {
-          position: "asc"
-        }
-      }
-    }
-  });
-
-  if (!validCourse || validCourse.Chapter.length === 0) {
-    return redirect("/");
-  }
-
-  return redirect(`/courses/${validCourse.id}/chapters/${validCourse.Chapter[0].id}`);
+export default function page() {
+  return (
+    <div>Course Page Coming</div>
+  )
 }
-
-export default CourseIdPage;
