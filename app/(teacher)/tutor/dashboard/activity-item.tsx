9d598e442ab154e518a1304af0f6cc51d3e024ff
Harmaton <njagiiharmaton@gmail.com>
2024-09-22 16:23:53 +0300

    Deploy the dashboard

--- app/(teacher)/tutor/dashboard/activity-item.tsx
+++ app/(teacher)/tutor/dashboard/activity-item.tsx
@@ -1,15 +1,18 @@
 import { User } from "lucide-react";
 
 const ActivityItem = () => (
-    <div className="flex items-start mb-4">
-      <div className="bg-orange-100 p-2 rounded-full mr-3">
-        <User className="text-orange-500" />
-      </div>
-      <div>
-        <p className="font-semibold">Kevin comments on your lecture "What is ux" in "2021 ui/ux design with figma"</p>
-        <p className="text-sm text-gray-500">Just now</p>
-      </div>
+  <div className="flex items-start mb-4">
+    <div className="bg-orange-100 p-2 rounded-full mr-3">
+      <User className="text-orange-500" />
     </div>
-  );
+    <div>
+      <p className="font-semibold">
+        Kevin comenta en tu sesion de terapia ¿Qué es la ansiedad? en Terapia
+        cognitivo-conductual 2021
+      </p>
+      <p className="text-sm text-gray-500">Hace 2 minutos</p>
+    </div>
+  </div>
+);
 
-  export default ActivityItem
+export default ActivityItem;
