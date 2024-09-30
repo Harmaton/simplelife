d12d3ebd6a23a257bd29d6d345c52140966c93ce
Harmaton <njagiiharmaton@gmail.com>
2024-09-30 11:00:05 +0300

    Added responsiveness to search page

--- app/search/categories.tsx
+++ app/search/categories.tsx
@@ -14,9 +14,8 @@ import {
   FcExternal,
 } from "react-icons/fc";
 import { GrProjects } from "react-icons/gr";
-import {IconType} from "react-icons"
+import { IconType } from "react-icons";
 import { CategoryItem } from "./category-item";
-
 
 interface CategoriesProps {
   items: Category[] | undefined;
@@ -28,20 +27,13 @@ const iconMap: Record<Category["name"], IconType> = {
   "Pack Desarrollo Cognitivo": FcExternal,
   "Pack Terapia Holística": FcSalesPerformance,
   "Pack Espiritualidad": FcCollaboration,
-  "Pack Psicologia":  FcPortraitMode
+  "Pack Psicologia": FcPortraitMode,
 };
 
 export const Categories = ({ items }: CategoriesProps) => {
   return (
     <div className="flex flex-col">
-      {/* <div className='border rounded-md m-auto p-4 bg-background mb-2 text-center w-full'> 
-      <h2 className="max-w-7xl   text-xl md:text-3xl font-bold text-neutral-800 dark:text-neutral-200 font-sans mb-3">
-      Certificaciones 
-      </h2>
-      <p className='text-sm mt-4 mb-2'>Sharpen Your Skills with Proffesional Packs of Diplomas and earn Certificatates</p>
-      </div> */}
-      
-      <div className="flex items-center gap-x-2 m-auto overflow-x-auto pb-2 mt-2">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 m-auto overflow-x-auto pb-2 mt-2">
         {items ? (
           items.map((item) => (
             <CategoryItem
@@ -52,7 +44,7 @@ export const Categories = ({ items }: CategoriesProps) => {
             />
           ))
         ) : (
-          <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+          <div className="col-span-full text-center">
             No Categories Added Yet
           </div>
         )}
